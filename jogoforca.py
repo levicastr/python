31b7d3047c14ad6087f12a9e91fe9a31bf1b8f8a
levicastr <63020217+levicastr@users.noreply.github.com>
2023-01-20 15:43:29 -0300

    Delete jogoforca.py

--- jogoforca.py
+++
@@ -1,9 +0,0 @@
-def jogar():
-    print("***********************************")
-    print("bem vindo ao jogo de forca!")
-    print("***********************************")
-
-    print("\nfim,voçê utiluzou todas as tentativas")
-
-if(__name__=="__main__"):
-    jogar()
